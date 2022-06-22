23ab221dc3c64d6dd84a2776d215d90273a4e272
serpro2130 <serproemcamseguridadprivada@gmail.com>
2022-06-22 12:50:58 -0500

    Primer commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+export const getGifs = async(category) => { 
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=eWNlIJTPsF0XVulhuNurjw6lO2iyJ3xJ&q=${ category }&limit=10`
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+  
+    const gifs = data.map( img => ({
+      id: img.id,
+      title: img.title,
+      url: img.images.downsized_medium.url
+    }));
+    
+    return gifs;
+  
+  }  
